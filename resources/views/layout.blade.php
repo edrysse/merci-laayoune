78e6397e0bc6ce7c1e8c50162239bc551c877c32
edrysse <117366780+edrysse@users.noreply.github.com>
2024-10-30 19:31:08 +0100

    secur files to https

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,14 +8,15 @@
     <title>my_site</title>
     <base href="/public">
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link rel="stylesheet" href="style.css">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"></script>
+    <link rel="stylesheet" href="{{ secure_asset('style.css') }}">
+
+    <!-- External CSS & JS (Secure Links) -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></script>
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
@@ -33,9 +34,10 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
         integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 </head>
+
 <nav class="navbar navbar-expand-lg bg-body-tertiary" style="background-color:bisque !important">
     <div class="container">
-        <a class="navbar-brand" href="{{ route('admin') }}">admin</a>
+        <a class="navbar-brand" href="{{ secure_url(route('admin')) }}">admin</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -43,25 +45,25 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="repas">menu</a>
+                    <a class="nav-link active" aria-current="page" href="{{ secure_url('repas') }}">menu</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="contact">contact</a>
+                    <a class="nav-link" href="{{ secure_url('contact') }}">contact</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="reservation">reservation</a>
+                    <a class="nav-link" href="{{ secure_url('reservation') }}">reservation</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="photos">galery</a>
+                    <a class="nav-link" href="{{ secure_url('photos') }}">gallery</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="user">users</a>
+                    <a class="nav-link" href="{{ secure_url('user') }}">users</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="blog">blogs</a>
+                    <a class="nav-link" href="{{ secure_url('blog') }}">blogs</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="chef">chefs</a>
+                    <a class="nav-link" href="{{ secure_url('chef') }}">chefs</a>
                 </li>
             </ul>
         </div>
@@ -72,24 +74,17 @@
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <!-- Left Side Of Navbar -->
-        <ul class="navbar-nav me-auto">
-
-        </ul>
-
-        <!-- Right Side Of Navbar -->
+        <ul class="navbar-nav me-auto"></ul>
         <ul class="navbar-nav ms-auto">
-            <!-- Authentication Links -->
             @guest
                 @if (Route::has('login'))
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        <a class="nav-link" href="{{ secure_url(route('login')) }}">{{ __('Login') }}</a>
                     </li>
                 @endif
-
                 @if (Route::has('register'))
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        <a class="nav-link" href="{{ secure_url(route('register')) }}">{{ __('Register') }}</a>
                     </li>
                 @endif
             @else
@@ -98,17 +93,12 @@
                         data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                         {{ Auth::user()->name }}
                     </a>
-
                     <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-
-
-                        <a class="dropdown-item" href="{{ route('logout') }}"
-                            onclick="event.preventDefault();
-                                         document.getElementById('logout-form').submit();">
+                        <a class="dropdown-item" href="{{ secure_url(route('logout')) }}"
+                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                             {{ __('Logout') }}
                         </a>
-
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        <form id="logout-form" action="{{ secure_url(route('logout')) }}" method="POST" class="d-none">
                             @csrf
                         </form>
                     </div>
@@ -119,16 +109,14 @@
 </nav>
 
 <body>
-    @yield('content');
+    @yield('content')
 
+    <!-- Footer Scripts (Secure Links) -->
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
     </script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
         integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
@@ -136,7 +124,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"
         integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous">
     </script>
-
 </body>
 
 </html>
